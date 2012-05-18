e0a94c3b56c6c81cc55ca909b83a00fa14224d45
wormboy <wormboy@localhost>
2012-05-18 04:11:39 -0700

    fix module error

--- statemachine.py
+++ statemachine.py
@@ -14,7 +14,7 @@ class TransitionError(FSMError):
 class RestartError(FSMError):
     pass
 
-class StateManager:
+class State:
     def __init__(self):
         self.states = {}
         self.state = None
