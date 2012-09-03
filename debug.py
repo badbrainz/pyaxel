eedf0362a69daee2f4f65dbb5d81c948ae835b26
wormboy <wormboy@localhost>
2012-09-02 19:19:44 -0700

    don't execute cmd for duplicate jobs. allow complex searches in jobqueue.

---
+++ debug.py
@@ -0,0 +1,34 @@
+## {{{ http://code.activestate.com/recipes/52215/ (r1)
+import sys
+import traceback
+def backtrace(debug_locals=True):
+    tb = sys.exc_info()[2]
+    if not tb:
+        print "No exception is being handled"
+        return
+    while 1:
+        if not tb.tb_next:
+            break
+        tb = tb.tb_next
+    stack = []
+    f = tb.tb_frame
+    while f:
+        stack.append(f)
+        f = f.f_back
+    stack.reverse()
+    print "Locals by frame, innermost last"
+    for frame in stack:
+        print
+        print "Frame %s in %s at line %s" % (frame.f_code.co_name,
+                                             frame.f_code.co_filename,
+                                             frame.f_lineno)
+        if debug_locals:
+            for key, value in frame.f_locals.items():
+                print "\t%20s = " % key,
+                try:
+                    print value
+                except:
+                    print "<ERROR WHILE PRINTING VALUE>"
+    print
+    traceback.print_exc()
+## end of http://code.activestate.com/recipes/52215/ }}}
