c359bf81bf42843da3fa0f40e3dfcb5c2a2b1fd4
wormboy <wormboy@localhost>
2012-09-15 07:58:33 -0700

    add partial metalink support

--- chromium_ext/js/io.js
+++ chromium_ext/js/io.js
@@ -1,11 +1,11 @@
 var io = {};
 
-io.ajax = function(url, callback) {
+io.http = function(url, callback, type) {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if (req.readyState == XMLHttpRequest.DONE)
             if (req.status == 200)
-                callback(req.responseText);
+                callback(!type ? req.responseXML : req.responseText);
     }
     req.open('GET', url, true);
     req.send();
